023dd1cd64c2cd17944644c69fbce19b82fae51b
Gustavo Wolfer <gustavowolfer@gmail.com>
2026-01-22 13:22:36 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar2.png")
+    img.setAttribute("alt", "Doutor foto filtro branco")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
