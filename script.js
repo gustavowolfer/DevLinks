947be044fa667ce4785179d7625cd1586f729869
Gustavo Wolfer <gustavowolfer@gmail.com>
2026-08-07 19:57:14 -0300

    feat: nova foto no light mode e adicionado link do linkedin

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar2.jpg")
+    img.setAttribute("src", "./assets/profile.png")
     img.setAttribute("alt", "Doutor foto filtro branco")
   } else {
     img.setAttribute("src", "./assets/avatar.jpg")
