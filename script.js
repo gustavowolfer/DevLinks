4d43d71a426d27fed2ad5fefa713652e562ea21a
Gustavo Wolfer <gustavowolfer@gmail.com>
2026-08-07 20:14:07 -0300

    fix: foto no html e no md

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/profile.png")
-    img.setAttribute("alt", "Doutor foto filtro branco")
+    img.setAttribute("src", "./assets/Profile.png")
+    img.setAttribute("alt", "Foto de perfil")
   } else {
     img.setAttribute("src", "./assets/avatar.jpg")
     img.setAttribute("alt", "Doutor foto")
